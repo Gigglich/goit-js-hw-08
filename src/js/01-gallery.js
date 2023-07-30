b5f13c35c56625aa5055fa5127e2e72abc3a4d73
Karina2023h <127620416+Karina2023h@users.noreply.github.com>
2023-07-30 11:51:05 +0200

    js8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const itemsGalleryCreat = createGalleryItems(galleryItems);
+gallery.insertAdjacentHTML('beforeend', itemsGalleryCreat);
+
+function createGalleryItems(item) {
+  return item
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: 'true',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: '250',
+});
 
 console.log(galleryItems);
